b11a2eab663a962a641b464d16a001f571906f0b
robyserpa <rserpa@est.ups.edu.ec>
2021-01-19 23:37:56 -0500

    Mantenimiento general

--- src/main/java/ec/edu/ups/springboot/repository/TelephoneRepository.java
+++ src/main/java/ec/edu/ups/springboot/repository/TelephoneRepository.java
@@ -1,7 +1,5 @@
 package ec.edu.ups.springboot.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,5 @@ import ec.edu.ups.springboot.model.Telephone;
 
 @Repository
 public interface TelephoneRepository extends JpaRepository<Telephone, Integer>{
-
-	public List<Telephone> findByUserId(int userId);
-
+	
 }
